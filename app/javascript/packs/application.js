a930d226d45c33f3aadd3052d1f29d2c3a78c940
Aizuddin Badry <aizuddinbadry@outlook.com>
2019-09-02 11:46:09 +0800

    integrating semantic ui sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require semantic-ui
 
 require("@rails/ujs").start();
 require("turbolinks").start();
