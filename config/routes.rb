d89b2c5a9a4d9a953560b3c3bc88b2175ab6e274
Aizuddin Badry <aizuddinbadry@outlook.com>
2019-09-02 11:20:43 +0800

    remove unused devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(:email => 'test@gmail.com', :password => '123qaz', :password_confirmation => '123qaz')
