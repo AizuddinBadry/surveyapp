c295abc3a42d5bc509a9fbd7652658283385cd22
Aizuddin Badry <aizuddinbadry@outlook.com>
2019-09-05 07:55:39 +0800

    move navbar to partial, added auth layout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  layout 'auth'
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
