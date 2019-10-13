2d1acea2d9a3c03546899a859683f0aa7e5bc456
Masyrafpauzi <acap_sum41@hotmail.com>
2019-10-13 18:07:30 +0800

    add logo survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,5 @@
 class Survey < ApplicationRecord
+  include ImageUploader.attachment(:image)
   validates_presence_of :title
   belongs_to :user
   has_many :question_groups, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_080924) do
+ActiveRecord::Schema.define(version: 2019_10_13_091912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2019_10_13_080924) do
     t.boolean "show_final_button", default: false
     t.text "start_button_text"
     t.boolean "final_button_to_start", default: false
+    t.text "image_data"
     t.index ["user_id"], name: "index_surveys_on_user_id"
   end
 
