8d82823bcdddcb41317a010f2a9e7fb450b57ca1
Aizuddin Badry <aizuddinbadry@outlook.com>
2019-11-07 13:21:36 +0800

    fixed bugs if save_answer value is null

--- app/models/survey_answer.rb
+++ app/models/survey_answer.rb
@@ -1,4 +1,15 @@
 class SurveyAnswer < ApplicationRecord
   belongs_to :survey
   belongs_to :question
+
+  def self.check_mc(question_pos, survey_id, session, answer)
+    @question = Question.find_by(survey_id: survey_id, survey_position: question_pos)
+    @save_answer = SurveyAnswer.where(session: session, question_id: @question.id, survey_id: survey_id).first
+    Rails.logger.info "?????????????????????????????#{session}, #{@question.id}, #{survey_id}, #{answer}"
+    if @save_answer.value.include? answer.to_s
+      return true
+    else
+      return false
+    end unless @save_answer.nil? || @save_answer.value.nil?
+  end
 end
