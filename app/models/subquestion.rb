c12ce68112a18a90929bb48b1d181e00e5d02844
Masyrafpauzi <acap_sum41@hotmail.com>
2019-11-14 00:47:55 +0800

    fix other language

--- app/models/subquestion.rb
+++ app/models/subquestion.rb
@@ -1,3 +1,20 @@
 class Subquestion < ApplicationRecord
   belongs_to :question
+
+
+  def default_values
+    size = Subquestion.where(question_id: self.question_id).size
+    self.position ||= size + 1
+  end
+
+  def self.query_language(answer, lang)
+    
+    @answer = answer.other_language["#{lang}"]
+    if @answer == false
+      return nil
+    else
+      return @answer
+    end
+  end
+  
 end
