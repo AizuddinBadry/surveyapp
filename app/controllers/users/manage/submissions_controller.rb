f1c0c19f3b68e69fc243895a2867837f12350d24
Masyrafpauzi <acap_sum41@hotmail.com>
2020-02-15 16:09:11 +0800

    add export csv

--- app/controllers/users/manage/submissions_controller.rb
+++ app/controllers/users/manage/submissions_controller.rb
@@ -6,11 +6,21 @@ class Users::Manage::SubmissionsController < Users::BaseController
 
     def show
         @submission = SurveyAnswer.joins(:survey,:question).where(survey_id: @survey.id).order(created_at: :desc)
+
+        respond_to do |format|
+            format.html
+            format.csv { send_data @submission.to_csv, filename: "Survey-Submission-#{@survey.title}.csv" }
+          end
     end
 
     def detail_submissions
         @breadcrumb = SurveyAnswer.joins(:survey,:question).where(session: params[:id]).first
         @submissions = SurveyAnswer.joins(:survey,:question).where(session: params[:id]).order(created_at: :asc)
+
+        respond_to do |format|
+            format.html
+            format.csv { send_data @submissions.to_csv, filename: "Survey-Submission-#{@breadcrumb.session}.csv" }
+          end
     end
 
     def new
